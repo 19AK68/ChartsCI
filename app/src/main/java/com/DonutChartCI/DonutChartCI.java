ad6ad55f2832e7991b7a9d7e5f64b2a646e94256
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2018-08-22 11:46:17 +0300

    first chart

--- app/src/main/java/com/DonutChartCI/DonutChartCI.java
+++ app/src/main/java/com/DonutChartCI/DonutChartCI.java
@@ -1,180 +1,309 @@
 package com.DonutChartCI;
 
-//import android.content.Context;
-//import android.content.res.TypedArray;
-//import android.graphics.BlurMaskFilter;
-//import android.graphics.Canvas;
-//import android.graphics.Paint;
-//import android.graphics.Path;
-//import android.graphics.RadialGradient;
-//import android.graphics.RectF;
-//import android.graphics.Shader;
-//import android.util.AttributeSet;
-//import android.view.View;
 
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RadialGradient;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Property;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
 
 import com.example.ak.user.chartsci.R;
 
-public class DonutChartCI extends View {
+public class DonutChartCI extends ViewGroup {
 
-    private float radius;
+    private static final long ANIM_DURATION_DEFAULT = 500;
+    private static final long ANIM_DURATION_LONG = 2000;
 
-    Paint paint;
-    Paint shadowPaint;
+    private DonutChartCIView donutChartCIView;
 
-    Path myPath;
-    Path shadowPath;
+    private Paint paintPrimary;
+    private Paint paintSecondary;
+    private Paint paintTextPrimary;
+    private Paint paintTextSecondary;
 
-    RectF outterCircle;
-    RectF innerCircle;
-    RectF shadowRectF;
+    private final RectF oval = new RectF();
+    private float width;
+
+    private float textSizePrimary;
+    private float textSizeSecondary;
+
+    private int colorPrimary;
+    private int colorSecondary;
+    private int colorTextPrimary;
+    private int colorTextSecondary;
+
+    private ObjectAnimator headAnimator;
+    private ObjectAnimator tailAnimator;
+
+    // Процент
+    // хранится внутри как float между 0..360 (в градусах)
+    // выставляется через сеттер и геттер как 0..100 (в процентах)
+
+    private float percentDeg;
+    public float getPercent() { return (percentDeg/ 360.f) * 100.f; }
+    public void setPercent(float percent) { percentDeg = ((percent % 100)/ 100.f) * 360.f; }
+    private final Property<DonutChartCI, Float> percentProperty = new Property<DonutChartCI, Float>(Float.class, "Percent") {
+        @Override public Float get(DonutChartCI fd) { return fd.getPercent(); }
+        @Override public void set(DonutChartCI fd, Float value) { fd.setPercent(value); }
+    };
+
+    //используется для анимации
+    // диапазон 0..360
+    // переполнение циклов с модулем смещение на 270deg, т.е. 0deg - 12 часов, 90deg - 3 часа
 
 
-    public DonutChartCI(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    private float originAngle = 0;
+    private float getOriginAngle() { return (originAngle + 270) % 360; }
+    private void setOriginAngle(Float value) { originAngle = (value % 360); }
+    private final Property<DonutChartCI, Float> originAngleProperty = new Property<DonutChartCI, Float>(Float.class, "OriginAngle") {
+        @Override public Float get(DonutChartCI fd) { return fd.getOriginAngle(); }
+        @Override public void set(DonutChartCI fd, Float value) { fd.setOriginAngle(value); }
+    };
 
-        TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.DonutChartCI,
-                0, 0
-        );
+    // аннулируем представление, поэтому он перерисовывается, когда аниматор изменяет свойство
 
+    private final ValueAnimator.AnimatorUpdateListener animatorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override public void onAnimationUpdate(ValueAnimator valueAnimator) { donutChartCIView.invalidate(); }
+    };
+
+    public DonutChartCI(Context context) {
+        super(context);
+        init();
+
+    }
+
+    public DonutChartCI(Context context,AttributeSet attrs) {
+        super(context,attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DonutChartCI, 0, 0);
+
+        // attempt to get any values from the xml
         try {
-            radius = a.getDimension(R.styleable.DonutChartCI_radius, 20.0f);
+            colorPrimary = a.getColor(R.styleable.DonutChartCI_fdColorPrimary, Color.rgb(225, 140, 80));
+            colorSecondary = a.getColor(R.styleable.DonutChartCI_fdColorSecondary, Color.rgb(200,200,200));
         } finally {
             a.recycle();
         }
 
-        paint = new Paint();
-        paint.setDither(true);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setStrokeJoin(Paint.Join.ROUND);
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setAntiAlias(true);
-        paint.setStrokeWidth(radius / 14.0f);
+        init();
 
-        shadowPaint = new Paint();
-        shadowPaint.setColor(0xf0000000);
-        shadowPaint.setStyle(Paint.Style.STROKE);
-        shadowPaint.setAntiAlias(true);
-        shadowPaint.setStrokeWidth(6.0f);
-        shadowPaint.setMaskFilter(new BlurMaskFilter(4, BlurMaskFilter.Blur.SOLID));
-
-        myPath = new Path();
-        shadowPath = new Path();
+    }
 
 
-        outterCircle = new RectF();
-        innerCircle = new RectF();
-        shadowRectF = new RectF();
 
-        float adjust = (.019f*radius);
-        shadowRectF.set(adjust, adjust, radius*2-adjust, radius*2-adjust);
+    private void init() {
 
-        adjust = .038f * radius;
-        outterCircle.set(adjust, adjust, radius*2-adjust, radius*2-adjust);
+        donutChartCIView = new DonutChartCIView (getContext());
+        addView(donutChartCIView);
 
-        adjust = .276f * radius;
-        innerCircle.set(adjust, adjust, radius*2-adjust, radius*2-adjust);
+        textSizePrimary = 10.f;
+        textSizeSecondary = 10.f;
 
+        colorTextPrimary = Color.BLACK;
+        colorTextSecondary = Color.BLACK;
+
+        // setup animator
+        headAnimator = new ObjectAnimator();
+        headAnimator.setTarget(this);
+        headAnimator.setProperty(percentProperty);
+        headAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        headAnimator.addUpdateListener(animatorUpdateListener);
+
+        // tail animator
+        // animates the 'end' of the ring.. for loading anim
+        tailAnimator = new ObjectAnimator();
+        tailAnimator.setTarget(this);
+        tailAnimator.setProperty(originAngleProperty);
+        tailAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        tailAnimator.addUpdateListener(animatorUpdateListener);
+        tailAnimator.setFloatValues(0, 1080);
+        tailAnimator.setDuration(DonutChartCI.ANIM_DURATION_LONG);
+        tailAnimator.setRepeatCount(Animation.INFINITE);
+        tailAnimator.setStartDelay(100);
+
+        // setup paint
+        paintPrimary = new Paint();
+        paintPrimary.setAntiAlias(true);
+        paintPrimary.setColor(colorPrimary);
+        paintPrimary.setStyle(Paint.Style.STROKE);
+        paintPrimary.setStrokeCap(Paint.Cap.ROUND);
+
+        paintSecondary = new Paint();
+        paintSecondary.setAntiAlias(true);
+        paintSecondary.setColor(colorSecondary);
+        paintSecondary.setStyle(Paint.Style.STROKE);
+
+        paintTextPrimary = new TextPaint();
+        paintTextPrimary.setAntiAlias(true);
+        paintTextPrimary.setColor(colorTextPrimary);
+        paintTextPrimary.setStyle(Paint.Style.STROKE);
+
+        paintTextSecondary = new TextPaint();
+        paintTextSecondary.setAntiAlias(true);
+        paintTextSecondary.setColor(colorTextSecondary);
+        paintTextSecondary.setStyle(Paint.Style.STROKE);
     }
 
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        // draw shadow
-        paint.setShader(null);
-        float adjust = (.0095f*radius);
-        paint.setShadowLayer(8, adjust, -adjust, 0xaa000000);
-        drawDonut(canvas,paint, 0,359.9f);
-
-
-        // green
-        setGradient(0xff84BC3D,0xff5B8829);
-        drawDonut(canvas,paint, 0,60);
-
-        //red
-        setGradient(0xffe04a2f,0xffB7161B);
-        drawDonut(canvas,paint, 60,60);
-
-        // blue
-        setGradient(0xff4AB6C1,0xff2182AD);
-        drawDonut(canvas,paint, 120,60);
-
-        // yellow
-        setGradient(0xffFFFF00,0xfffed325);
-        drawDonut(canvas,paint, 180,180);
-
-
-
-    }
-
-    public void drawDonut(Canvas canvas, Paint paint, float start,float sweep){
-
-        myPath.reset();
-        myPath.arcTo(outterCircle, start, sweep, false);
-        myPath.arcTo(innerCircle, start+sweep, -sweep, false);
-        myPath.close();
-        canvas.drawPath(myPath, paint);
-    }
-
-    public void setGradient(int sColor, int eColor){
-        paint.setShader(new RadialGradient(radius, radius, radius-5,
-                new int[]{sColor,eColor},
-                new float[]{.6f,.95f}, Shader.TileMode.CLAMP) );
+    protected void onLayout(boolean b, int i, int i2, int i3, int i4) {
+        // do nothing
+        // don't call super.onLayout() -- this would cause a layout pass on the children
+        // children will lay out in onSizeChanged()
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
 
-        int desiredWidth = (int) radius*2;
-        int desiredHeight = (int) radius*2;
+        // account for padding
+        float xpad = (float) (getPaddingLeft() + getPaddingRight());
+        float ypad = (float) (getPaddingTop() + getPaddingBottom());
 
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        // figure out how big the doughnut can be
+        float ww = (float) w - xpad;
+        float hh = (float) h - ypad;
+        float diameter = Math.min(ww, hh);
 
-        int width;
-        int height;
+        oval.set(0.f, 0.f, diameter, diameter);
+        oval.offsetTo(getPaddingLeft(), getPaddingTop());
 
-        //70dp exact
-        if (widthMode == MeasureSpec.EXACTLY) {
-            width = widthSize;
-        }else if (widthMode == MeasureSpec.AT_MOST) {
-            //wrap content
-            width = Math.min(desiredWidth, widthSize);
-        } else {
-            width = desiredWidth;
-        }
+        // set stroke width..
+        width = diameter / 15.f;
+        paintPrimary.setStrokeWidth(width);
+        paintSecondary.setStrokeWidth(width);
 
-        //Measure Height
-        if (heightMode == MeasureSpec.EXACTLY) {
-            height = heightSize;
-        } else if (heightMode == MeasureSpec.AT_MOST) {
-            height = Math.min(desiredHeight, heightSize);
-        } else {
-            height = desiredHeight;
-        }
-
-        //MUST CALL THIS
-        setMeasuredDimension(width, height);
+        // lay out the child view that actually draws the doughnut
+        donutChartCIView.layout((int) oval.left, (int) oval.top, (int) oval.right, (int) oval.bottom);
     }
+
+
+    //region Animations
+
+    private void animateRing(float from, float to, long duration) {
+        headAnimator.setFloatValues(from, to);
+        headAnimator.setDuration(duration);
+        headAnimator.start();
+    }
+
+
+
+    public void animateSetPercent(float percent) {
+        float old = getPercent();
+        setPercent(percent);
+        animateRing(old, getPercent(), DonutChartCI.ANIM_DURATION_DEFAULT);
+
+    }
+
+    /**
+     * gracefully stop the "indeterminate loading" animation and reset to the initial point
+     */
+    public void stopAnimateLoading() {
+        stopAnimateLoading(.1f);
+    }
+
+    /**
+     * gracefully stop the "indeterminate loading" animation and animate to a new {@code percent}
+     * @param percent the new percent to animate to once current animation has finished
+     */
+    public void stopAnimateLoading(final float percent) {
+        if (headAnimator.isRunning() && tailAnimator.isRunning()) {
+            // remove any listeners we may have previously attached
+            headAnimator.removeAllListeners();
+            tailAnimator.removeAllListeners();
+
+            // gracefully let the animation finish it's current cycle, then kill it and remove this listener
+            tailAnimator.addListener(new Animator.AnimatorListener() {
+                @Override public void onAnimationStart(Animator animation) {}
+                @Override public void onAnimationEnd(Animator animation) {}
+                @Override public void onAnimationCancel(Animator animation) {}
+                @Override public void onAnimationRepeat(Animator animation) {
+                    animation.end();
+                    animation.removeListener(this);
+                    setOriginAngle(0.f);
+                }
+            });
+
+            headAnimator.addListener(new Animator.AnimatorListener() {
+                @Override public void onAnimationStart(Animator animation) {}
+                @Override public void onAnimationEnd(Animator animation) {}
+                @Override public void onAnimationCancel(Animator animation) {}
+                @Override public void onAnimationRepeat(Animator animation) {
+                    animation.end();
+                    animation.removeListener(this);
+                    // reset the head animator values we changed when starting the loading animation
+                    headAnimator.setRepeatCount(0);
+                    headAnimator.setDuration(DonutChartCI.ANIM_DURATION_DEFAULT);
+
+                    animateSetPercent(percent);
+                }
+            });
+
+        }
+    }
+
+    /**
+     * start the "indeterminate loading" animation
+     */
+    public void startAnimateLoading() {
+        headAnimator.setFloatValues(.1f, 66.f, .1f);
+        headAnimator.setDuration(DonutChartCI.ANIM_DURATION_LONG);
+        headAnimator.setRepeatCount(Animation.INFINITE);
+
+        headAnimator.start();
+        tailAnimator.start();
+    }
+
+
+
+    //endregion
+
+    /**
+     * DonutChartCIView class
+     */
+    class DonutChartCIView extends View {
+
+        private final RectF fdvOval = new RectF();
+
+        public DonutChartCIView(Context ctx) {
+            super(ctx);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            //canvas.drawText("PAINT TEXT PRIMARY", oval.left, oval.top, paintTextPrimary);
+
+            // draw the background doughnut
+            canvas.drawArc(fdvOval, 0, 360, false, paintSecondary);
+
+            // draw the main ring on top
+            canvas.drawArc(fdvOval, getOriginAngle(), percentDeg, false, paintPrimary);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            fdvOval.set(width, width /*[SIC]*/, w - width, h - width);
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/ak/user/chartsci/MainActivity.java
+++ app/src/main/java/com/example/ak/user/chartsci/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.ak.user.chartsci;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.DonutChartCI.DonutChartCI;
+
 public class MainActivity extends AppCompatActivity {
+    public DonutChartCI donutChartCI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        donutChartCI = (DonutChartCI) findViewById(R.id.donutChartCI);
+
+        donutChartCI.startAnimateLoading();
+
     }
 }
