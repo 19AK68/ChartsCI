43f3968d21efdbd60d60b392a41b2e133360ec3b
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2018-08-27 18:10:23 +0300

    add param

--- app/src/main/java/com/DonutChartCI/DonutChartCI.java
+++ app/src/main/java/com/DonutChartCI/DonutChartCI.java
@@ -15,9 +15,13 @@ import android.view.View;
 
 import com.example.ak.user.chartsci.R;
 
+import java.util.ArrayList;
+
 public class DonutChartCI extends View {
 
-    private float radius ;
+
+
+    private float radius;
 
     Paint paint;
     Paint shadowPaint;
@@ -30,6 +34,7 @@ public class DonutChartCI extends View {
     RectF shadowRectF;
 
 
+
     public DonutChartCI(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -37,6 +42,8 @@ public class DonutChartCI extends View {
                 attrs,
                 R.styleable.DonutChartCI,
                 0, 0
+
+
         );
 
         try {
@@ -69,14 +76,17 @@ public class DonutChartCI extends View {
         innerCircle = new RectF();
         //  shadowRectF = new RectF();
 
-        float adjust = (.019f*radius);
+        float adjust = (.019f * radius);
 //        shadowRectF.set(adjust, adjust, radius*2-adjust, radius*2-adjust);
 
         adjust = .038f * radius;
-        outterCircle.set(adjust, adjust, radius*2-adjust, radius*2-adjust);
+        outterCircle.set(adjust, adjust, radius * 2 - adjust, radius * 2 - adjust);
 
         adjust = .276f * radius;
-        innerCircle.set(adjust, adjust, radius*2-adjust, radius*2-adjust);
+        innerCircle.set(adjust, adjust, radius * 2 - adjust, radius * 2 - adjust);
+
+
+
 
     }
 
@@ -84,34 +94,32 @@ public class DonutChartCI extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
         float sumTotalMortgage = 100000.0f;
         float sumMarketVaule = 125000.0f;
-        float sumTotalEquity  = sumMarketVaule- sumTotalMortgage ;
+        float sumTotalEquity = sumMarketVaule - sumTotalMortgage;
 
 
-
-        float prTotalEquity = ((sumMarketVaule/ sumTotalMortgage) -1 )*180.0f;
-        float prTotalMortgage =  (1- prTotalEquity )*180.0f;
-        float prMarketVaule =   (sumMarketVaule/sumMarketVaule)*180.f;
-
-
-
-
-
+        float prTotalEquity = ((sumMarketVaule / sumTotalMortgage) - 1) * 180.0f;
+        float prTotalMortgage = (1 - prTotalEquity) * 180.0f;
+        float prMarketVaule = (sumMarketVaule / sumMarketVaule) * 180.f;
 
 
         // draw shadow
         paint.setShader(null);
-        float adjust = (.0095f*radius);
+        float adjust = (.0095f * radius);
         paint.setShadowLayer(8, adjust, -adjust, 0xffffffff);
-        drawDonut(canvas,paint, 0,359.9f);
+        drawDonut(canvas, paint, 0, 359.9f);
+
 
 
 
 
         // green TotalEquity
-        setGradient(0xffffffff,0xffFF8C00);; //setGradient(0xff84BC3D,0xff98CA47);
-        drawDonut(canvas,paint, 45,-267.25f);// drawDonut(canvas,paint, 0,-prTotalEquity);
+        setGradient(0xffffffff, 0xffFF8C00);
+        ; //setGradient(0xff84BC3D,0xff98CA47);
+        drawDonut(canvas, paint, 45, -267.25f);// drawDonut(canvas,paint, 0,-prTotalEquity);
 
         //red
 
@@ -124,8 +132,8 @@ public class DonutChartCI extends View {
         //    drawDonut(canvas,paint, -prTotalEquity,(prTotalEquity-180));
 
 
-        setGradient(0xffffffff,0xffffffff); //setGradient(0xff4AB6C1,0xff2182AD);
-        drawDonut(canvas,paint, 45,92.75f);
+        setGradient(0xffffffff, 0xffffffff); //setGradient(0xff4AB6C1,0xff2182AD);
+        drawDonut(canvas, paint, 45, 92.75f);
 
 
 //        // blue
@@ -137,30 +145,31 @@ public class DonutChartCI extends View {
 //        drawDonut(canvas,paint, 180,180);
 
 
-
     }
 
-    public void drawDonut(Canvas canvas, Paint paint, float start,float sweep){
+
+
+    public void drawDonut(Canvas canvas, Paint paint, float start, float sweep) {
 
         myPath.reset();
         myPath.arcTo(outterCircle, start, sweep, false);
-        myPath.arcTo(innerCircle, start+sweep, -sweep, false);
+        myPath.arcTo(innerCircle, start + sweep, -sweep, false);
         myPath.close();
         canvas.drawPath(myPath, paint);
     }
 
-    public void setGradient(int sColor, int eColor){
-        paint.setShader(new RadialGradient(radius, radius, radius-5,
-                new int[]{sColor,eColor},
-                new float[]{.6f,.95f}, Shader.TileMode.CLAMP) );
+    public void setGradient(int sColor, int eColor) {
+        paint.setShader(new RadialGradient(radius, radius, radius - 5,
+                new int[]{sColor, eColor},
+                new float[]{.6f, .95f}, Shader.TileMode.CLAMP));
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        int desiredWidth = (int) radius*2;
-        int desiredHeight = (int) radius*2;
+        int desiredWidth = (int) radius * 2;
+        int desiredHeight = (int) radius * 2;
 
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
@@ -173,7 +182,7 @@ public class DonutChartCI extends View {
         //70dp exact
         if (widthMode == MeasureSpec.EXACTLY) {
             width = widthSize;
-        }else if (widthMode == MeasureSpec.AT_MOST) {
+        } else if (widthMode == MeasureSpec.AT_MOST) {
             //wrap content
             width = Math.min(desiredWidth, widthSize);
         } else {
@@ -191,6 +200,18 @@ public class DonutChartCI extends View {
 
         //MUST CALL THIS
         setMeasuredDimension(width, height);
+    }
+
+    public void  initParamChart(float price1, float price2) {
+
+        float f1,f2,f3;
+
+        f1 = price1;
+        f2 = price2;
+        f3 = f2-f1;
+
+
+
     }
 
 
--- app/src/main/java/com/example/ak/user/chartsci/MainActivity.java
+++ app/src/main/java/com/example/ak/user/chartsci/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.ak.user.chartsci;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.DonutChartCI.DonutChartCI;
+
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    public static int CHART_FULL_CIRCLE = 1 ;
+    public static int CHART_INCOMPLECTE_CIRCLE = 2 ;
+    Context context;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        float paramOne =100000.0f;
+        float param2 =125000.0f;
+
+//        DonutChartCI donutChartCI = new DonutChartCI(context);
+//        donutChartCI.initParamChart(paramOne,param2);
+
     }
+
+
+
+
+
+
 }
